e697586ef3787a2efca7162ebf73d1c6a6f17bc4
João Pedro A.P.P. Barbosa <jpandradepaesdev@gmail.com>
2025-11-10 20:28:10 -0300

    ADDED integração com firebase
    O firebase está adicionado e funcionando, nenhum componente se comunica com ele ainda

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,17 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const environment = {
-  production: false
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcqRvwuMLxQBS5OIGjLvtE7dXFeWaLJGM',
+  authDomain: 'gest-pro-7d600.firebaseapp.com',
+  projectId: 'gest-pro-7d600',
+  storageBucket: 'gest-pro-7d600.firebasestorage.app',
+  messagingSenderId: '843063877155',
+  appId: '1:843063877155:web:bd28ab6bf6b86eaebe9b96',
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ import {
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -20,5 +23,17 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideHttpClient(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'gest-pro-7d600',
+        appId: '1:843063877155:web:bd28ab6bf6b86eaebe9b96',
+        storageBucket: 'gest-pro-7d600.firebasestorage.app',
+        apiKey: 'AIzaSyAcqRvwuMLxQBS5OIGjLvtE7dXFeWaLJGM',
+        authDomain: 'gest-pro-7d600.firebaseapp.com',
+        messagingSenderId: '843063877155',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 });
